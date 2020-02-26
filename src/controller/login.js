3f8da80c8890afa2c1700640106954b3dd53576b
Tatas Fachrul <tatasfachrul@gmail.com>
2020-02-26 11:13:33 +0700

    standard fix login controller

--- src/controller/login.js
+++ src/controller/login.js
@@ -1,6 +1,6 @@
 require('dotenv/config')
 
-const jwt_decode = require('jwt-decode')
+const jwtDecode = require('jwt-decode')
 const moment = require('moment')
 const loginModel = require('../model/login')
 const { response } = require('../helpers/helpers')
@@ -16,15 +16,16 @@ module.exports = {
     try {
       const UserData = await loginModel.getUsername(data.username)
       const result = await validateLogin(res, data, UserData)
-      // response(res, 200, result)
+      console.log(result.result)
+      response(res, 200, result)
     } catch (error) {
       console.log(error)
-      console.log(result.result)
+      response(res, 400, error)
     }
   },
   proLogout: (req, res) => {
     const token = req.headers['x-access-token']
-    var decoded = jwt_decode(token)
+    var decoded = jwtDecode(token)
     const idUser = decoded.id_user
     const updateDt = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')
 
