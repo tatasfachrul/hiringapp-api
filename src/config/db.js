4a50e91fe6c0945d2702b84626186648d3944c3c
Tatas Fachrul <tatasfachrul@gmail.com>
2020-02-26 15:03:35 +0700

    add port to mysql configuration

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,8 @@ const pool = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 })
 
 pool.connect((err) => {
