2370a73594208959be0c74ab212f3c5c3d32a54c
Tatas Fachrul <tatasfachrul@gmail.com>
2020-02-26 19:17:44 +0700

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'kotlin-api',
-    script: './server.js',
+    script: 'server.js',
     error_file: '~/Logs/error_logs_kotlin.log',
     out_file: '~/Logslogs_kotlin.log'
   }]
